c77317aab62c37425432e4a8f0d80afd3f23c876
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-21 14:11:18 +0000

    Deploy TheDan64/inkwell to github.com/TheDan64/inkwell.git:gh-pages

---
+++ aliases.js
@@ -0,0 +1,15 @@
+var ALIASES = {};
+ALIASES['either'] = {};
+ALIASES['enum_methods'] = {};
+ALIASES['inkwell'] = {};
+ALIASES['inkwell_internal_macros'] = {};
+ALIASES['libc'] = {};
+ALIASES['llvm_sys'] = {};
+ALIASES['proc_macro2'] = {};
+ALIASES['quote'] = {};
+ALIASES['quote'] = {};
+ALIASES['syn'] = {};
+ALIASES['syn'] = {};
+ALIASES['synom'] = {};
+ALIASES['unicode_xid'] = {};
+ALIASES['unicode_xid'] = {};
